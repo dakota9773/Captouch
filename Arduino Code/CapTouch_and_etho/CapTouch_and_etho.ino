3b353830de037e3ee886d655d021619793ba4eb5
Dakota Nipper <dakota.nipper97@gmail.com>
2025-04-07 15:33:44 -0400

    Updating for new test
    Updated the code to allow it to interface with ethovision and my python program. Also simplified the MPR121 initialization

--- Arduino Code/CapTouch_and_etho/CapTouch_and_etho.ino
+++ Arduino Code/CapTouch_and_etho/CapTouch_and_etho.ino
@@ -13,57 +13,29 @@
   const int releaseThreshold = 1;
   unsigned long elapsedMillis;  
   unsigned long runningstartMillis = 0;
-  const int touchPins[3] = {5, 6, 7};
-  const int ledPins[3] = {A1, A2, A4};
-  String cap_touch[3] = {"0", "0", "0"};
+  const int touchPins[2] = {3, 9};
+  const int ledPins[2] = {A1, A4};
+  String cap_touch[2] = {"0", "0"};
   int EthoPin = A3;
-  bool SavingData = false;
 
 void setup() {
   //Define Baud rate and pins
     Serial.begin(BAUD_RATE);
     pinMode(ledPins[1], OUTPUT);
     pinMode(ledPins[2], OUTPUT);
-    pinMode(ledPins[3], OUTPUT);
     pinMode(EthoPin, INPUT);
 
   //MPR121 Initialization and settings
     if (!MPR121.begin(MPR121_ADDR)) {
-      Serial.println("error setting up MPR121");
-      switch (MPR121.getError()) {
-        case NO_ERROR:
-         Serial.println("no error");
-         break;
-        case ADDRESS_UNKNOWN:
-          Serial.println("incorrect address");
-          break;
-        case READBACK_FAIL:
-          Serial.println("readback failure");
-          break;
-        case OVERCURRENT_FLAG:
-          Serial.println("overcurrent on REXT pin");
-          break;
-        case OUT_OF_RANGE:
-          Serial.println("electrode out of range");
-          break;
-        case NOT_INITED:
-          Serial.println("not initialised");
-          break;
-        default:
-          Serial.println("unknown error");
-          break;
-      }
-      while (1);
+        Serial.println("Error setting up MPR121");
+        while (1);
     }
     MPR121.setInterruptPin(MPR121_INT);
-
     MPR121.setTouchThreshold(touchThreshold);
     MPR121.setReleaseThreshold(releaseThreshold);
-
     MPR121.setFFI(FFI_18);
     MPR121.setSFI(SFI_18);
     MPR121.setGlobalCDT(CDT_2US);
-
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     //MPR121.autoSetElectrodes();
@@ -76,11 +48,11 @@ void loop() {
   int ethopin = digitalRead(EthoPin);
   MPR121.updateAll();
 
-  for (int i = 1; i < 3; i++) {
-    if (MPR121.isNewTouch(i)) {
+  for (int i = 0; i < 2; i++) {
+    if (MPR121.isNewTouch(touchPins[i])) {
       cap_touch[i] = "1";
       digitalWrite(ledPins[i], HIGH);
-    } else if (MPR121.isNewRelease(i)) {
+    } else if (MPR121.isNewRelease(touchPins[i])) {
       cap_touch[i] = "0";
       digitalWrite(ledPins[i], LOW);
     }
@@ -102,7 +74,6 @@ void loop() {
     }
     cap_data += ",";
     cap_data += String(ethopin);
-
     Serial.println(cap_data);
   }
 }
